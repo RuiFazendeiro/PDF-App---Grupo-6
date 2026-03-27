412630759aa6e9a21c6c544b0cffceda0662ecc6
amrcorreia <andreia.ap@gmail.com>
2026-03-27 12:12:23 +0000

    Alterar nomes das propriedades da classe Relatorio de EN para PT

--- SimProgramming.Model/Relatorio.cs
+++ SimProgramming.Model/Relatorio.cs
@@ -2,13 +2,13 @@
 namespace SimProgramming.Model;
 public class Relatorio : DocumentoBase
 {
-    public required string Author { get; set; }
-    public required string Content { get; set; }
+    public required string Autor { get; set; }
+    public required string Conteudo { get; set; }
 
     public override bool Validate()
     {
         return base.Validate()
-            && !string.IsNullOrWhiteSpace(Author)
-            && !string.IsNullOrWhiteSpace(Content);
+            && !string.IsNullOrWhiteSpace(Autor)
+            && !string.IsNullOrWhiteSpace(Conteudo);
     }
 }
